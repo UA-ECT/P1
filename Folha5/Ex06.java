6a7cfb4287123a9a854979f8c4d13c03dd045154
Rodrigo Faria <74474900+JRodrigoFaria@users.noreply.github.com>
2020-11-14 23:52:19 +0000

    Update Ex06.java

--- Folha5/Ex06.java
+++ Folha5/Ex06.java
@@ -1,34 +1,36 @@
 import java.util.Scanner;
 
-public class Ex06 {
+public class Ex06
+{
     public static final Scanner KB = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.print("\n\nIntroduza número: ");
-        int n = getIntRange(1, 99);
-
-        System.out.print("\n\n");
-        drawTabuada(n);
-        System.out.print("\n\n");
+    public static void main(String[] args)
+    {
+        System.out.print("Insert a number from 1 to 99: ");
+        int n = sc.nextInt();
+        table(n);
     }
     
-
-    public static int getIntRange(int min, int max){
-        int n = KB.nextInt();
-        while( n<min||n>max ){
-            System.out.printf("Número inválido. Introduza inteiro (%d a %d): ",min,max);
-            n = KB.nextInt();
-        }
-        return n;
+    public static boolean getIntRange(int n)
+    {
+        return ((n>0) && (n<=100));
     }
-
-    public static void drawTabuada(int n){
-        System.out.print("-------------------\n");
-        System.out.printf("|  Tabuada dos %2d |\n", n);
-        System.out.print("-------------------\n");
-        for(int i=1; i<=10 ;i++){
-            System.out.printf("| %2d x %2d  |  %3d |\n", n, i, n*i);
+    
+    public static void table(int n)
+    {
+        if (getIntRange(n))
+        {
+        draw();
+        System.out.printf("\n| Tabuada dos  %02d |\n", n);
+        draw();
+        System.out.printf("\n");
+        for (int i=1; i<=10; i++) System.out.printf("| %2d x %2d |  %3d  |\n", n, i, (n*i));
+        draw();
         }
-        System.out.print("-------------------");
+    }
+    
+    public static void draw()
+    {
+        for (int i=0; i<19; i++) System.out.print("-");
     }
 }
