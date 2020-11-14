2dae46855e0693e934de9bbd9da46b38fa92d25f
Rodrigo Faria <74474900+JRodrigoFaria@users.noreply.github.com>
2020-11-14 23:57:35 +0000

    Update Ex03.java

--- Folha5/Ex03.java
+++ Folha5/Ex03.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Ex03
 {
-    public static final Scanner KB = new Scanner(System.in);
+    public static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args)
     {
