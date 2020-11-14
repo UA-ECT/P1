2e58fef068999ee01b2193f99e1331f55c4ef0a4
Rodrigo Faria <74474900+JRodrigoFaria@users.noreply.github.com>
2020-11-14 23:53:47 +0000

    Update Ex04.java

--- Folha5/Ex04.java
+++ Folha5/Ex04.java
@@ -3,31 +3,16 @@ import java.util.Scanner;
 public class Ex04 {
     public static final Scanner KB = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.print("\nIntroduza n (1 a 10): ");
-        int m = getIntRange(1, 10);
-
-        for(int i=m; i>0 ;i--){
-            System.out.printf("\n%d! = %d", i, fact(i));
-        }
-
-        System.out.print("\n\n");
+    public static void main(String[] args)
+    {
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++) System.out.printf("%d! = %d\n", i, fact(i));
     }
-
-    public static int getIntRange(int min, int max){
-        int n = KB.nextInt();
-        while( n<min||n>max ){
-            System.out.printf("Número inválido. Introduza inteiro (%d a %d): ",min,max);
-            n = KB.nextInt();
-        }
-        return n;
-    }
-
-    public static int fact(int n){
-        int fact = 1;
-        for(int i=1;i<=n;i++){
-            fact *= i;
-        }
-        return fact;
+    
+    public static int fact(int i)
+    {
+        int factor = 1;
+        for(int x=1; x<=i; x++) factor *= x;
+        return factor;
     }
 }
