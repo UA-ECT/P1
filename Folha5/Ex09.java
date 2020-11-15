7a026fcdaa23b0e963f0c126a3c8ef00c8b450bb
Rodrigo Faria <74474900+JRodrigoFaria@users.noreply.github.com>
2020-11-15 00:00:04 +0000

    Update Ex09.java

--- Folha5/Ex09.java
+++ Folha5/Ex09.java
@@ -1,39 +1,19 @@
 import java.util.Scanner;
 
 public class Ex09 {
-    public static final Scanner KB = new Scanner(System.in);
+    public static final Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.print("\n\nIntroduza m: ");
-        int m = getIntRange(1, Integer.MAX_VALUE);
-
-        System.out.print("\nNúmeros Primos: \n");
-        for(int i=1; i<=m ;i++){
-            if( isprime(i) ){
-                System.out.println(i);
-            }
-        }
-        System.out.print("\n\n");
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Insert a number: ");
+        int m = sc.nextInt();
+        for(int i=1; i<=m; i++) if (isPrime(i)) System.out.printf("%d\n", i);
     }
-
-    public static boolean isprime(int n){
-        double counter = 1;
-        for(int i=2; i<n ;i++){
-            counter *= n%i;
-        }
-        if( counter!=0 ){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public static int getIntRange(int min, int max){
-        int n = KB.nextInt();
-        while( n<min||n>max ){
-            System.out.printf("Número inválido. Introduza inteiro (%d a %d): ",min,max);
-            n = KB.nextInt();
-        }
-        return n;
+    
+    public static boolean isPrime(int i)
+    {
+        for (int k=2; k<i; k++) if (i%k==0) return false;
+        return true;
     }
 }
