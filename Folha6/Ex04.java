e35f1eb0e9969777a5adcd0a53b8bd46eba6d577
JRodrigoFaria <jrfaria@ua.pt>
2020-12-03 17:57:12 +0000

    sou lindo

--- Folha6/Ex04.java
+++ Folha6/Ex04.java
@@ -1,44 +1,60 @@
 import java.util.Scanner;
 
-public class Ex04 {
-    public static final Scanner KB = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        System.out.print("\n\nIntroduza número de notas que pretende utilizar: ");
-        int n = getIntRange(1, Integer.MAX_VALUE);
-
-        int notas[] = new int[21];
-        int max = 0;
-        for(int i=0; i<n ;i++){
-            System.out.printf("Nota %2dº: ", i+1);
-            int a = getIntRange(1, 20);
-            notas[a]++;
-            if( notas[a]>max ) max = notas[a];
-        }
-
-        System.out.print("\nHistograma de notas");
-        System.out.print("\n-------------------------------------------------------");
-        for(int i=20; i>=0 ;i--){
-            System.out.printf("\n%2d | ", i);
-            printNchar('*', (int)Math.round(((double)notas[i]/(double)max)*50));
-        }
-        
-
-        System.out.print("\n\n");
+public class P64 
+{
+    public static Scanner sc = new Scanner(System.in);
+    
+    public static void main (String[] args)
+    {
+        printer(getWell(makeHistogram(getArray(getSize()))));
     }
-
-    public static void printNchar(char c, int n){
-        for(int i=0;i<n;i++){
-            System.out.print(c);
+    
+    public static int getSize()
+    {
+        System.out.print("Insira a quantidade de notas a analisar: ");
+        return sc.nextInt();
+    }
+    
+    public static int[] getArray(int size)
+    {
+        int[] array= new int[size];
+        System.out.printf("Insira os valores do seu array: \n");
+        for(int i=0;i<array.length; i++) array[i]= sc.nextInt();
+        return array;
+    }
+    
+    public static int[] makeHistogram(int[] array)
+    {
+        int[] histogram= new int[21];
+        for(int i=0; i<histogram.length; i++) histogram[i]=0;
+        for(int k=0; k<array.length; k++) histogram[array[k]]++;
+        return histogram;
+    }
+    
+    public static void printer(int[] histogram)
+    {
+        System.out.printf("\n Histograma de notas\n");
+        for(int i=0; i<50; i++) System.out.print("-");
+        System.out.println("");
+        for(int i=20; i>=0; i--) 
+        {
+            System.out.printf(" %2d | ",i);
+            for(int k=0; k<histogram[i]; k++) System.out.print("*");
+            System.out.println("");
         }
     }
-
-    public static int getIntRange(int min, int max){
-        int n = KB.nextInt();
-        while( n<min||n>max ){
-            System.out.printf("Número inválido. Introduza inteiro (%d a %d): ",min,max);
-            n = KB.nextInt();
-        }
-        return n;
-    }
+    
+    public static int getMax(int[] histogram)
+    {
+		int max= histogram[0];
+		for(int i=0; i<histogram.length; i++) if (max<histogram[i]) max= histogram[i];
+		return max;
+	}
+	
+	public static int[] getWell(int[] histogram)
+	{
+		int max = getMax(histogram);
+		for (int i=0; i<histogram.length; i++) histogram[i]= (int)((histogram[i] * 50)/max);
+		return histogram;
+	}
 }
